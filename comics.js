bdfa51eb45e7ecc17bf1021431e08daf93f50c85
J Hicks <29406916+jentacularJava@users.noreply.github.com>
2025-07-20 18:17:31 -0400

    bug fix

---
+++ comics.js
@@ -0,0 +1,448 @@
+// Auto-generated comic data for SPA
+const comics = [
+  {
+    title: 'Dentist',
+    filename: 'Dentist.JPG',
+    caption: 'Any excuse to lie down by myself.'
+  },
+  {
+    title: 'Falsies',
+    filename: 'Falsies.jpg',
+    caption: 'Oh, you mean those falsies.'
+  },
+  {
+    title: '',
+    filename: 'IMG_0001.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0002.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0003.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0004.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0005.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0006.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0007.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0009.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0010.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0011.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0012.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0013.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0014.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0015.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0016.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0017.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0019.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0020.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0026.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0028.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0030.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0031.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0032.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0034.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0036.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0041.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0042.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0044.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0045.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0046.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0047.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0049.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0051.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0059.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0062.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0064.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0067.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0069.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0074.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0081.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0085.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0155.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0160.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0161.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0164.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0172.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0174.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0175.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0183.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0184.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0186.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0188.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0190.jpg',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0191.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0192.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0193.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0195.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0196.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0197.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0198.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0199.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0200.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0201.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0204.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0206.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0207.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0208.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0210.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0212.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0213.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0214.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0219.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0222.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0223.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0225.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0227.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0228.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0244.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0251.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0252.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0254.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0256.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0258.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0259.PNG',
+    caption: ''
+  },
+  {
+    title: '',
+    filename: 'IMG_0260.PNG',
+    caption: ''
+  },
+  {
+    title: 'Leftovers',
+    filename: 'Leftovers.PNG',
+    caption: 'Why am I like this?.'
+  },
+  {
+    title: 'Smells',
+    filename: 'Smells.png',
+    caption: 'Kids do not hold back.'
+  },
+];
